36552fe15e832aedbb37622f1df341d8e3936074
Hongxu Liu <djshu.us@gmail.com>
2013-09-27 18:00:58 -0700

    Corrected test condition to check for insert method, not children method.

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -6,7 +6,7 @@ describe("binarySearchTree", function() {
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
   });
